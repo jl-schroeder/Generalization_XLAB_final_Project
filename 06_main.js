ddfb098225a263fa801643549965631865469606
luca-schröder <slotter12@gmail.com>
2019-07-15 16:32:11 +0200

    changed from debug to directLink => ready for poblishing

--- 06_main.js
+++ 06_main.js
@@ -25,12 +25,12 @@ $("document").ready(function() {
         ],
         // Here, you can specify all information for the deployment
         deploy: {
-            experimentID: "INSERT_A_NUMBER",
+            experimentID: "79",
             serverAppURL: "https://babe-demo.herokuapp.com/api/submit_experiment/",
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
-            deployMethod: "debug",
+            deployMethod: "directLink",
             contact_email: "janlschroede@uos.de",
             prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
         },
